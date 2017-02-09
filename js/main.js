c0a62df0d5eaffd3fb7e56884e5163de3bb50865
Data Detective <datadetectiveagent007@gmail.com>
2017-02-08 22:18:17 -0800

    testing API call

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-﻿// Your code here!
+﻿function twitch() {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/search/streams?query=starcraft", createStr);
+    //createStr(test);
+}
+
+function createStr(data) {
+    prtAlert(data);
+}
+
+function prtAlert(msg) {
+    document.getElementById("main").innerHTML = JSON.stringify(msg, null, 3)
+    console.log(JSON.stringify(msg, null, 3));
+}
+
+twitch();
