1ea0d16350a491c8a72df7e9fa0e4eae5fdae78a
Data Detective <datadetectiveagent007@gmail.com>
2017-02-23 10:26:53 -0800

    Update to API Call

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
-﻿function twitch() {
-    $.getJSON("https://wind-bow.gomix.me/twitch-api/search/streams?query=starcraft", createStr);
-    //createStr(test);
-}
+﻿var type = ["channels", "streams"];
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-function createStr(data) {
-    prtAlert(data);
+function twitch() {
+    users.forEach(function(user) {
+        type.forEach(function (name) {
+            $.getJSON('https://wind-bow.gomix.me/twitch-api/' + name + '/' + user + '?callback=?', prtAlert);
+        });
+    });
 }
 
 function prtAlert(msg) {
-    document.getElementById("main").innerHTML = JSON.stringify(msg, null, 3)
+    document.getElementById("main").innerHTML = JSON.stringify(msg, null, 3);
     console.log(JSON.stringify(msg, null, 3));
 }
 
-twitch();
+$(document).ready(function () {
+    twitch();
+});
